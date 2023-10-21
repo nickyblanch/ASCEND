416ca15189f6fbc40f5494af46589a697a8f596e
Nicolas Blanchard <nickyblanch@email.arizona.edu>
2023-10-20 17:50:55 -0700

    example for geiger

--- Fall 2023/Code/src/geiger.hpp
+++ Fall 2023/Code/src/geiger.hpp
@@ -8,8 +8,16 @@
 // https://github.com/MonsieurV/ArduinoPocketGeiger/tree/master/src
 #include <RadiationWatch.h>
 
+// Create a gegier counter object
+RadiationWatch geiger_object;
+
 // Initialization function: called one time, sets up Geiger counter
 // Call during setup in main
+void setup_geiger(void) {
+
+  geiger_object.setup();
+  
+}
 
 
 // Get data function
@@ -23,3 +31,17 @@
   // Increment variable storing radiation count
   // radiation_count++;
 //}
+
+///////////////////////////////////////////////////////////////
+// Generic function
+// <return data type> <function name> ( <argument 1, arument 2, etc) {
+
+// }
+// int test_function(float number_1, int number_2) {
+
+  // return numeric_result;
+// }
+
+// Struct
+// Data type that can store multiple variables
+// Sensor that needs to store 3 integers and a string

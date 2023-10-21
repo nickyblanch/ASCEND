1883853988d1473a8564286bb5ddc4bb76e7336d
LioraWilkins <148596105+LioraWilkins@users.noreply.github.com>
2023-10-20 21:17:51 -0400

    Worked a bit on the Geiger Counter code

--- Fall 2023/Code/src/geiger.hpp
+++ Fall 2023/Code/src/geiger.hpp
@@ -8,20 +8,22 @@
 // https://github.com/MonsieurV/ArduinoPocketGeiger/tree/master/src
 #include <RadiationWatch.h>
 
-// Create a gegier counter object
-RadiationWatch geiger_object;
+// Creating RadiationWatch Object for Setup:
+
+RadiationWatch geigerObject;
 
 // Initialization function: called one time, sets up Geiger counter
 // Call during setup in main
-void setup_geiger(void) {
-
-  geiger_object.setup();
-  
+void geiger_counter_setup() {
+  geigerObject.setup();
 }
-
-
+  
+    
 // Get data function
 // Call during loop in main
+void geiger_counter_loop() {
+  geigerObject.loop()
+}
 
 
 // Callback function
@@ -31,17 +33,3 @@ void setup_geiger(void) {
   // Increment variable storing radiation count
   // radiation_count++;
 //}
-
-///////////////////////////////////////////////////////////////
-// Generic function
-// <return data type> <function name> ( <argument 1, arument 2, etc) {
-
-// }
-// int test_function(float number_1, int number_2) {
-
-  // return numeric_result;
-// }
-
-// Struct
-// Data type that can store multiple variables
-// Sensor that needs to store 3 integers and a string
